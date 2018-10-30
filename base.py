20acf8d7f498849510b0dd34723d8d590338b42c
Arsalaan Ansari <aaa2325@columbia.edu>
2018-10-29 23:24:56 -0400

    Added basic quick_panel to plugin that populates rows with static data from list

--- base.py
+++ base.py
@@ -2,6 +2,25 @@
 import sublime_plugin
 
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class InsertPanelCommand(sublime_plugin.TextCommand):
+    # Checks user input on panel
+    def on_click(self, index):
+
+            # Esc key don't do anything
+            # If cancel is pressed return -1
+
+        if index == -1:
+            return
+
+        self.view.run_command("insert_my_text",
+                              {"args": {'text': self.data[index]}})
+
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        # Populating panel with names of group members
+        self.data = ["Arsalaan", "Raphael", "Kevin", "Trevor"]
+        self.view.window().show_quick_panel(self.data, self.on_click, 1, 2)
+
+
+class InsertText(sublime_plugin.TextCommand):
+    def run(self, edit, args):
+        self.view.insert(edit, self.view.sel()[0].begin(), args['text'])
