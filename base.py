fad5a231d3a4fa681c9cb020dabe4ca7d2e236d4
Arsalaan Ansari <aaa2325@columbia.edu>
2018-10-30 02:14:50 -0400

    Display popup of comment on Sublime text window when panel row is clicked.

--- base.py
+++ base.py
@@ -1,10 +1,20 @@
 # import sublime
-#travis test
+# travis test
 import sublime_plugin
 
 
 class InsertPanelCommand(sublime_plugin.TextCommand):
     # Checks user input on panel
+
+    # Panel Row clicked generates HTML popup on screen
+    def gen_comment(self, text):
+        title_str = text
+        point = self.view.text_point(30, 5)
+        html_str = '<b>' + title_str + \
+            '</b><br><a href="test">Click Me</a> <style>color=red</style>'
+        self.view.show_popup(html_str, on_navigate=print, location=point)
+
+    # Handles panel click logic
     def on_click(self, index):
 
             # Esc key don't do anything
@@ -15,14 +25,12 @@ class InsertPanelCommand(sublime_plugin.TextCommand):
 
         self.view.run_command("insert_my_text",
                               {"args": {'text': self.data[index]}})
+        self.gen_comment(self.data[index])
+
+    # Entrypoint
 
     def run(self, edit):
         # Populating panel with names of group members
         self.data = ["Arsalaan", "Raphael", "Kevin", "Trevor"]
         self.view.window().show_quick_panel(self.data, self.on_click, 1, 2)
         return self.data
-
-
-class InsertText(sublime_plugin.TextCommand):
-    def run(self, edit, args):
-        self.view.insert(edit, self.view.sel()[0].begin(), args['text'])
