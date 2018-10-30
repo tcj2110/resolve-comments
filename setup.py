ffe4cad54e53e9d597a7d3176cb8f98ca2d13c66
Raphael Junior <raphnano@gmail.com>
2018-10-30 13:23:14 -0400

    Sets up authentication and access to repos. Watch out for the .json hack.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Resolve-comments',
+      version='0.0.1',
+      description='Resolve comments faster',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
