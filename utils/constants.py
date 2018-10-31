d50866fd9d472a273b1981e2613354eba58a3e30
Raphael Junior <raphnano@gmail.com>
2018-10-30 21:33:45 -0400

    All hacks removed.

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,5 @@
 
 GITHUB_USER_URL = 'https://api.github.com/user'
+GITHUB_AUTH = 'https://api.github.com/authorizations/clients/:4f435d9f7beb896578b9'
+REPOS = GITHUB_USER_URL + '/repos'
+GITHUB_REPO = "https://api.github.com/repos/"
