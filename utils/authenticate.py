17e2212e1c2630d29b775f6e24c13d3152110fe6
Raphael Junior <raphnano@gmail.com>
2018-11-08 11:08:37 -0500

    All hacks removed.

--- utils/authenticate.py
+++ utils/authenticate.py
@@ -40,7 +40,7 @@ class Authenticate:
         response = requests.get(url, auth=(self.username, self.token), params=params)
         return response.json()
 
-    def get_pr_reviews(self, owner, repo, pr_id):   
+    def get_pr_reviews(self, owner, repo, pr_id):
     #returns a list of reviews underneath a PR.
         url = 'https://api.github.com/repos/:owner/:repo/pulls/:number/reviews'
         list = []
@@ -48,28 +48,19 @@ class Authenticate:
         url+=  "/pulls/:"+str(pr_id)+"/reviews"
         response = requests.get(url, auth=(self.username, self.token), params=params)
         return response.json()
-        """
-        ##for later in the semester, this function will return a list
-        implemented = False
-        return list
-        """
 
     def get_pr_comments(self, owner, repo, pr_id):
-<<<<<<< HEAD
-
         implemented = False
         url = 'https://api.github.com/repos/:owner/:repo/pulls/:number/reviews'
         #returns a list of comments
         url = "https://api.github.com/repos/:" + str(owner)+  "/:" +str(repo)
         url+=  "/pulls/:"+str(pr_id)+"/reviews"
-=======
         url = git_constants.GITHUB_REPO + ("%s/%s/pulls/%s/comments" % (owner, repo, pr_id))
         params = {
             "sort": "created",
             "direction": "desc"
         }
         print(url)
->>>>>>> c9a694db5a9fb87b0c346f5fb00e7fa4ae3a8a10
         response = requests.get(url, auth=(self.username, self.token), params=params)
         return response.json()
 
