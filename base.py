acb4e5be3a25cf4bad2ffbe741808f5800790fc7
Arsalaan Ansari <aaa2325@columbia.edu>
2018-11-10 17:46:22 -0500

    Added functionality for quick_panel to load pull request data from github repository given a token, username, and repository name. Wrote testcase to ensure panel data being presented on the panel is parsed correctly.

--- base.py
+++ base.py
@@ -1,13 +1,32 @@
 
-# from .utils import authenticate
+from .utils import authenticate
 import sublime_plugin
 import sublime
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
+
 TOKEN = 'Nanosoft1*'
 USER = 'raphaeljunior'
+
+
+# Placeholder method for fetching comment data
+# Will be replaced with Github API comment data
+# Returns data in list format
+def load_quick_panel_data(token, user):
+    auth = authenticate.Authenticate(token, user)
+    # auth.load_repos()
+    # repos = auth.repos
+    # print(repos)
+    data = []
+    pull_requests = auth.get_pull_requests('ADI-Labs', 'culpa2')
+    for req in pull_requests:
+        title = req['title']
+        body = req['body']
+        content = [title, body]
+        data.append(content)
+    return data
 
 
 class InsertPanelCommand(sublime_plugin.TextCommand):
@@ -32,21 +51,9 @@ class InsertPanelCommand(sublime_plugin.TextCommand):
         print(cursor)
         self.view.sel().add(sublime.Region(cursor))
 
-    # Placeholder method for fetching comment data
-    # Will be replaced with Github API comment data
-    # Returns data in list format
-    def load_comment_data(self):
-        # auth = authenticate.Authenticate(TOKEN, USER)
-        # auth.load_repos()
-        # repos = auth.repos
-        # print(repos)
-        # pr_comments = auth.get_pr_comments('ADI-Labs', 'culpa2', 2)
-        # print(pr_comments)
-        data = [["Arsalaan", "wow"]] * 4
-        return data
-
     # Generates simple list HTML for comment data
     # Returns: HTML
+
     def gen_comment_html(self):
         data = self.load_comment_data()
         html_arr = [
@@ -68,7 +75,7 @@ class InsertPanelCommand(sublime_plugin.TextCommand):
     #  comments in side popup
 
     def gen_comment_list(self):
-        data = self.load_comment_data()
+        data = load_quick_panel_data(TOKEN, USER)
         self.view.window().show_quick_panel(data, self.on_click, 1, 2)
 
     # Entrypoint for plugin
