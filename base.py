b23c298926ebf7b3268680863494555aaed94114
Arsalaan Ansari <aaa2325@columbia.edu>
2018-11-27 00:27:30 -0500

    Completed username and token input panel functionality. When a user starts plugin they will be prompted with inputting username and token. Inputs are validated by Github and panel data will only be shown if both credentials are correct. Functionality to remember username and token for session needs to be implemented.

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
 
 import sublime_plugin
-# import sublime
+import sublime
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
@@ -9,24 +9,15 @@ from utils import authenticate  # noqa: E402
 TOKEN = 'Nanosoft1*'
 USER = 'raphaeljunior'
 
+# Class provides the entrypoint for the plugin.
+# All helper functions are external to
+# allow for easy unit testing.
+
 
 class InsertPanelCommand(sublime_plugin.TextCommand):
 
-    # Method opens up input window and prompts user for token and username
-
-    def user_token_input(self):
-        self.view.window().show_input_panel(
-            caption="Credential Prompt",
-            initial_text="Username:",
-            on_done=None,
-            on_change=None,
-            on_cancel=None)
-
-        return TOKEN, USER
-
     # Generates simple list HTML for comment data
-    # Returns: HTML
-
+    '''
     def gen_comment_html(self):
         data = self.load_comment_data()
         html_arr = [
@@ -39,39 +30,95 @@ class InsertPanelCommand(sublime_plugin.TextCommand):
         html_arr.append("</ul>")
         html_str = "".join(html_arr)
         return html_str
-
-    def on_click(self, index):
-        if(index == -1):
-            return -1
-
-    #  Manager function that uses helper functions to show
-    #  comments in side popup
-
-    def gen_comment_list(self):
-        data = load_quick_panel_data(self.token, self.user)
-        self.view.window().show_quick_panel(data, self.on_click, 1, 2)
+    '''
 
     # Entrypoint for plugin
 
     def run(self, edit):
-        self.token, self.user = self.user_token_input()
-        self.gen_comment_list()
+        username_input()
 
-# Placeholder method for fetching comment data
-# Will be replaced with Github API comment data
-# Returns data in list format
+
+# Method opens up input window and prompts username.
+# When user presses enter on_done function calls
+# token_input.
+
+def username_input():
+    def on_done(user_string):
+        user = user_string.strip()
+        token_input(user)
+
+    def on_change(user_string):
+        pass
+
+    def on_cancel(user_string):
+        print("User cancelled input")
+
+    sublime.active_window().show_input_panel(
+        caption="Username:",
+        initial_text="",
+        on_done=on_done,
+        on_change=on_change,
+        on_cancel=on_cancel)
+
+
+# Method opens up input window and prompts token.
+# When user presses enter on_done function calls
+# gen_comment_list.
+
+def token_input(user):
+    def on_done(token_string):
+        token = token_string.strip()
+        gen_comment_list(token, user)
+
+    def on_change(token_string):
+        pass
+
+    def on_cancel(token_string):
+        print("User cancelled input")
+
+    sublime.active_window().show_input_panel(
+        caption="Token:",
+        initial_text="",
+        on_done=on_done,
+        on_change=on_change,
+        on_cancel=on_cancel)
+
+#  Manager function that uses helper functions to show
+#  comments in side popup
+
+
+def gen_comment_list(token, user):
+    data = load_quick_panel_data(token, user)
+    sublime.active_window().show_quick_panel(data, on_click, 1, 2)
+
+# Method that responds to clicking quickpanel item
+
+
+def on_click(index):
+    if(index == -1):
+        return -1
+
+# Method loads Github pull_requests data by authenticating the user,
+# retrieving
 
 
 def load_quick_panel_data(token, user):
     auth = authenticate.Authenticate(token, user)
-    # auth.load_repos()
-    # repos = auth.repos
-    # print(repos)
     data = []
     pull_requests = auth.get_pull_requests('Raphaeljunior', 'resolve-comments')
+    if('message' in pull_requests and
+            pull_requests['message'] == 'Bad credentials'):
+        sublime.active_window().show_input_panel(
+            caption="Error Prompt",
+            initial_text="Error: Token or Username is Incorrect",
+            on_done=None,
+            on_change=None,
+            on_cancel=None)
+        return []
     for req in pull_requests:
         title = req['title']
         body = req['body']
-        content = [title, body]
+        user = req['user']['login']
+        content = [title, body, user]
         data.append(content)
     return data
