7ac14f0517959f3c405c6e978cd826744e6f5327
Arsalaan Ansari <aaa2325@columbia.edu>
2018-11-28 03:25:10 -0500

    Added functionality to add text to new view in split window.

--- base.py
+++ base.py
@@ -99,15 +99,18 @@ def on_click(index):
     if(index == -1):
         return -1
 
-    print("LAYOUT")
-    sublime.active_window().run_command(
-        'set_layout', {
-            "cols": [
-                0.0, 0.66, 1.0], "rows": [
-                0.0, 1.0], "cells": [
-                    [
-                        0, 0, 1, 1], [
-                            1, 0, 2, 1]]})
+    sublime.active_window().set_layout({
+        "cols": [
+            0.0, 0.66, 1.0], "rows": [
+            0.0, 1.0], "cells": [
+            [
+                0, 0, 1, 1], [
+                1, 0, 2, 1]]})
+    for numGroup in range(sublime.active_window().num_groups()):
+        if len(sublime.active_window().views_in_group(numGroup)) == 0:
+            sublime.active_window().focus_group(numGroup)
+            createdView = sublime.active_window().new_file()
+            createdView.run_command("insert", {"characters": "Hello"})
 
 
 # Method loads Github pull_requests data by authenticating the user and token.
